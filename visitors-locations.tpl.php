0967a86b6a0409db1f133f93d026e8cf70d2dfbb
xlyz <xlyz@livenup.it>
2011-05-02 14:08:12 +0200

    initial import

---
+++ visitors-locations.tpl.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<div class="content">
+  <object width="100%" height="300" type="application/x-shockwave-flash" data="<?php print $piwik_url ?>/libs/open-flash-chart/open-flash-chart.swf?v2i" id="UserCountrygetCountryChart">
+    <param name="allowScriptAccess" value="always"/>
+    <param name="wmode" value="opaque"/>
+    <param name="flashvars" value="data-file=<?php print $data1_url ?>"/>
+  </object>
+</div>
---
+++ visitors-times.tpl.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<h2><?php print t('Visit per local time') ?></h2>
+<div class="content">
+  <object width="100%" height="300" type="application/x-shockwave-flash" data="<?php print $piwik_url ?>/libs/open-flash-chart/open-flash-chart.swf?v2i" id="VisitTimegetVisitInformationPerLocalTimeChart">
+    <param name="allowScriptAccess" value="always"/>
+    <param name="wmode" value="opaque"/>
+    <param name="flashvars" value="data-file=<?php print $data1_url ?>"/>
+  </object>
+</div>
